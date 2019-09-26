5d399cc16a5bc67400999c2eeebf28466dea6cec
Pablo Osorio <pablo.osorio@xmarts.com>
2019-09-26 16:19:24 -0500

    update

--- stock_diference_jarochito/models/models.py
+++ stock_diference_jarochito/models/models.py
@@ -3,10 +3,10 @@
 from odoo import models, fields, api
 
 class StockMoveRoute(models.Model):
-	"""Agrega tabla para diferencias de entrega al stock.move"""
+	"""Agrega tabla para diferencias de entrega al stock.picking"""
 	_name = 'stock.move.route'
 
-	product_id = fields.Many2many(comodel_name='product.product', string='Producto Cargado')
+	product_id = fields.Many2one('product.product', string='Producto Cargado')
 	
 	return_qty = fields.Float(
 	    string='Cantidad Retornada',
@@ -17,16 +17,27 @@ class StockMoveRoute(models.Model):
 	diference_qty = fields.Float(
 	    string='Diferencia',
 	)
+	stock_picking_id = fields.Many2one("stock.picking")
 	
-class StockMove(models.Model):
-	_inherit = 'stock.move'
-
 
 class StockPicking(models.Model):
 	"""Agrega campos al stock.picking"""
 	_inherit = 'stock.picking'
 	is_to_route = fields.Boolean(string='Para Ruta')
 	user_route_id = fields.Many2one(comodel_name='res.user', string='Usuario de ruta')
+	route_moves = fields.One2many('stock.move.route','stock_picking_id', string='Tabla Diferencias')
+
+	@api.depends('move_ids_without_package')
+	@api.onchange('move_ids_without_package')
+	def _onchange_moves_picking(self):
+		for x in self.move_ids_without_package:
+			line_ids = []
+            res = {'value':{
+                    'route_moves':[],
+                }
+            }
+			print("PRODUCTO >>>>>>>>>>>>>>>>>> ",x.product_id.name)
+
 
 class PosSession(models.Model):
 	_inherit = 'pos.session'
