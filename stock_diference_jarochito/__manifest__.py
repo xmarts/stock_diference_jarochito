3c7a972fe3a2e1f391f998e8f7725d33e528b7be
victor manuel alonso soto <victor.alonso@xmarts.com>
2020-10-04 22:22:52 -0500

    update report corte de caja

--- stock_diference_jarochito/__manifest__.py
+++ stock_diference_jarochito/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','point_of_sale','stock','product','credit_limit_alert','hr','tracking'],
+    'depends': ['base','point_of_sale','stock','product','credit_limit_alert','hr','tracking','report_qweb_element_page_visibility'],
 
     # always loaded
     'data': [
